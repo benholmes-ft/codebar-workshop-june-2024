00eab6e5ec9918df27e1ef570d4ee02319640009
Ben Holmes <benjamin.holmes@ft.com>
2024-06-21 13:50:40 +0100

    added the name element

--- public/main.js
+++ public/main.js
@@ -1,26 +1,31 @@
 // init socket.io
 const socket = io();
  
-// Get the input and button elements
-const input = document.querySelector('input');
+// Get the inputs and button elements
+const messageInput = document.querySelector('#message');
 const button = document.querySelector('button');
+const nameInput = document.querySelector('#name');
 
 // Function to handle sending a message
 function sendMessage() {
     // Get the message text from the input
-    const message = input.value;   
+    const message = messageInput.value;   
 
-    socket.emit('newMessage', message);
+    socket.emit('newMessage', {
+        sender: nameInput.value,
+        message: message
+    });
 
     // Clear the input field
-    input.value = '';
+    messageInput.value = '';
 }
 
-function createNewMessage() {
+function createNewMessage(data) {
     // Create a new message element
+    const { sender, message } = data;
     const messageElement = document.createElement('div');
     messageElement.classList.add('message');
-    messageElement.textContent = data;
+    messageElement.textContent = sender + ': ' + message;
 
     // Append the new message element to the chat container
     const chatContainer = document.querySelector('.message-container');
@@ -31,7 +36,7 @@ function createNewMessage() {
 button.addEventListener('click', sendMessage);
 
 // Add event listener to the input for sending a message when Enter key is pressed
-input.addEventListener('keydown', function(event) {
+messageInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         sendMessage();
     }
